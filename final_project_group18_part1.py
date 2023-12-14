28c497bc858f7260dcde66130bc3f3870e295bf3
bvenn1023 <144082597+bvenn1023@users.noreply.github.com>
2023-12-13 22:34:22 -0500

    Update final_project_group18_part1.py

--- final_project_group18_part1.py
+++ final_project_group18_part1.py
@@ -17,7 +17,7 @@ import getpass
 #insert your email and password to the sender 
 sender_email = "autoemail1.3@gmail.com"
 sender_password = "pljo isun jxnw necn"
-# SMTP server details 
+
 smtp_server = 'smtp.gmail.com'
 smtp_port = 587
 
@@ -37,53 +37,40 @@ def find_compromised_files(username):
         print(f"Error executing find command: {result.stderr}")
         return []
 
-# Dislpay compromised files to command prompt
-def display_files(compromised_files):
-    for file_path in compromised_files:
-        print(file_path)
-
 def send_email(sender_email, sender_app_password, recipient_email, compromised_files, username):
-   #create a MIME multipart message for the email
     msg = MIMEMultipart()
     msg['From'] = sender_email
     msg['To'] = recipient_email
     msg['Subject'] = 'File Monitoring Report'
 
-#compose the email body
     body = f"Dear CTO,\n\nThe following files have been compromised for user {username}:\n\n{', '.join(compromised_files)}\n\nBest regards,\nYour File Monitoring Script"
     msg.attach(MIMEText(body, 'plain'))
-#attach comopromised files to the email
+
     for file_path in compromised_files:
         with open(file_path, 'rb') as file:
             part = MIMEApplication(file.read(), Name=os.path.basename(file_path))
             part['Content-Disposition'] = f'attachment; filename="{os.path.basename(file_path)}"'
             msg.attach(part)
-            
 
-#connect to the SMTP server and send the email
         with smtplib.SMTP(smtp_server, smtp_port) as server:
             server.starttls()
             server.login(sender_email, sender_app_password)
             server.sendmail(sender_email, recipient_email, msg.as_string())
 
-def download_files_sftp(compromised_files, download_path, ip_address, username, password):
-    transport = paramiko.Transport((ip_address, 22))
-    transport.connect(username=username, password=password)
-    sftp = paramiko.SFTPClient.from_transport(transport)
-
-    try:
+def download_files_ftp(compromised_files, download_path, ip_address, username, password):
+    with FTP(ip_address) as ftp:
+        ftp.login(username, password)
+        
         for file_path in compromised_files:
             file_name = os.path.basename(file_path)
-            destination_path = os.path.join(download_path, file_name) if download_path else file_name
-
-            # Download the file using SFTP
-            sftp.get(file_path, destination_path)
-    finally:
-        sftp.close()
-        transport.close()
+            local_file_path = os.path.join(download_path, file_name)
+            
+            with open(local_file_path, 'wb') as local_file:
+                ftp.retrbinary(f'RETR {file_path}', local_file.write)
+            
+            print(f"Downloaded {file_name} to {local_file_path}")
 
 def main():
-#parse command-line arguments 
     parser = argparse.ArgumentParser(description='File Monitoring Script')
     parser.add_argument('ip_address', help='IP address of the target computer')
     parser.add_argument('username', help='Username for the account on the target computer')
@@ -91,7 +78,7 @@ def main():
     parser.add_argument('-d', '--disp', action='store_true', help='Display the contents of affected files')
     parser.add_argument('-e', '--email', required=True, help='Email address of the CTO')
     parser.add_argument('-p', '--path', help='Download path for affected files')
-    parser.add_argument('-H', '--hlp', action='help', help='Show this help message and exit')  
+    parser.add_argument('-H', '--hlp', action='help', help='Show this help message and exit')
 
     args = parser.parse_args()
 
@@ -100,16 +87,18 @@ def main():
     recipient_email = args.recipient_email
     display_files_flag = args.disp
     download_path = args.path
-# find compromised files based on the provided username
+
     compromised_files = find_compromised_files(username)
-# display compromised files if the displayed flag is set
+
     if display_files_flag:
-        display_files(compromised_files)
-# send an email with the list of compromised files to the CTO
+        print("Affected Files:")
+        for file_path in compromised_files:
+            print(file_path)
+
     send_email(sender_email, sender_password, recipient_email, compromised_files, username)
-# download compromised files if the download path is provided 
+
     if download_path:
-        download_files_sftp(compromised_files, download_path, ip_address, username, getpass.getpass(prompt='Enter your password: '))
+        download_files_ftp(compromised_files, download_path, ip_address, username, "your_ftp_password")
 
 if __name__ == "__main__":
     main()
